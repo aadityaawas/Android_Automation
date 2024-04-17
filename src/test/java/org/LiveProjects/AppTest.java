a2808af048d28efc9b16b603daed42fc17df625e
aadityaawas <aadityaawas@gmail.com>
2024-04-17 23:47:47 +0530

    First commit

---
+++ src/test/java/org/LiveProjects/AppTest.java
@@ -0,0 +1,14 @@
+package org.LiveProjects;
+
+
+import org.testng.annotations.Test;
+
+
+public class AppTest 
+{
+    @Test
+    public void testcase1(){
+        System.out.println("Welcome to Live Projects");
+    }
+
+}
